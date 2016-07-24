d25a8ac60161b7574a3719720ea89f647a9610c8
kmandyam <Komputergeek1984!>
2016-07-23 21:01:53 -0700

    initial commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,9 @@
+app.controller('MainController', ['$scope', 
+	function($scope) { 
+  		$scope.title = 'Welcome to my angular checklisting app!'; 
+  		$scope.promo = 'This is a place for doctors to input their checklists and re-read them';
+  		$scope.master = {};
+  		$scope.update = function(task) {
+  			$scope.master = angular.copy(task);
+  		}
+}]);
