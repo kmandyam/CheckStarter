34cd5f518107ce4607beea31bfde3b0594bccb0c
kmandyam <karishma.mandyam@gmail.com>
2016-07-24 11:41:47 -0700

    INIT

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', 
 	function($scope) { 
   		$scope.title = 'Welcome to my angular checklisting app!'; 
-  		$scope.promo = 'This is a place for doctors to input their checklists and re-read them';
+  		$scope.promo = 'This is a place for doctors to input checklists and re-read them';
   		$scope.master = {};
   		$scope.update = function(task) {
   			$scope.master = angular.copy(task);
